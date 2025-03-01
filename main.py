cf59e441fe82b409ec1bfe284508c50e0ec1c46b
Zhe Wang <jameswangzhe1110@gmail.com>
2025-03-01 17:25:24 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.metrics import mean_squared_error
+import xgboost as xgb
+import lightgbm as lgb
+
